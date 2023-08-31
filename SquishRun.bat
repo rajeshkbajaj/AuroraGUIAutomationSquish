9ae057b03cefd9a5fd4abc15ca72303fbadd832e
Rajesh <98638681+rajeshkbajaj@users.noreply.github.com>
2023-08-31 11:59:27 +0530

    Add files via upload
    Updated one

--- SquishRun.bat
+++ SquishRun.bat
@@ -1,16 +1,13 @@
-@echo off
+REM add Squish to PATH
+set SQUISH_PREFIX=C:\Squish for Qt 7.1.1
+set PATH=%SQUISH_PREFIX%\bin;%PATH%
 
-set TEST_SUITE="C:\Users\rajesh.bajaj\suite_Rajesh"
-set TEST_CASE1="tst_case1-SanityGUICheck"
-set TEST_CASE2="tst_case6-ValueComparision"
 
-:: Squish
-set SQUISH_PATH="C:\Squish for Qt 7.1.1"
+REM The Log Directory does not (but perhaps should) have today's date in it:
+set LOGDIRECTORY=%USERPROFILE%\squish-results
 
-%SQUISH_PATH%\bin\squishrunner.exe ^
-    --testsuite %TEST_SUITE% ^
-    --testcase %TEST_CASE1% ^
-    --testcase %TEST_CASE2% ^
-    --local
+REM Execute the test suite with a --local squishserver:
+squishrunner --testsuite %USERPROFILE%\suite_Rajesh --local --reportgen "xml3.5,%LOGDIRECTORY%"
+
+testcentercmd --url=http://localhost:8800 --token=2Qiu0-_hADyqmaYlhN1nkBYOHvM4aeUnqg302ZVDOow upload Aurora %USERPROFILE%\squish-results --label=OS-WINDOWS --batch=2.0
     
-	
